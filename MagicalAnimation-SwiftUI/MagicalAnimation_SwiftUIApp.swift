2968eda7c3643f94ac0108f908cb2326ad6dbc03
Abdul Sami Sultan <56970013+AbdulSami-Sultan@users.noreply.github.com>
2023-09-25 21:01:40 +0500

    initial Commit

---
+++ MagicalAnimation-SwiftUI/MagicalAnimation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicalAnimation_SwiftUIApp.swift
+//  MagicalAnimation-SwiftUI
+//
+//  Created by Abdul Sami Sultan on 25/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MagicalAnimation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
